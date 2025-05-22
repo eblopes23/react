71e8765171b7b7ab29d9de2ede5637bea7c8bfc0
eblopes23 <eblopes23@gmail.com>
2025-05-22 20:27:22 -0300

    Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className='min-h-[80vh]'>
-        <Home />
-      </div>
-      
-      <Footer />
+       <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider> 
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
